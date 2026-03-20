fe4d75bd170a6debaf13c93283ef6cf6ac2f439a
Saksham Jain <saksham.khatod27@gmail.com>
2026-03-20 15:20:07 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,17 +15,17 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
           <link rel="icon" href="/icon.png" />
-          
-      </head>
-      <body className={`${inter.className} antialiased`}>
-        <TrackerIdentify />
-        {children}
-        <script
+          <script
   data-tracker-id="f53c5ee8-7afd-4cda-8782-4a606b722e7b"
   data-domain="twoseven.sakshamjain.dev"
   strategy="afterInteractive" 
   src="https://twoseven.sakshamjain.dev/tracker.js">
 </script>
+      </head>
+      <body className={`${inter.className} antialiased`}>
+        <TrackerIdentify />
+        {children}
+        
       </body>
     </html>
   );
